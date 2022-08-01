cc350cc8f658b606c423ef0f47b26e57d5332f62
Jake <astrospacemoth@gmail.com>
2022-07-31 22:47:21 -0400

    image fix 6

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ body.onload = function get_random_image(){
 
     selected_image = image_array[random_index]
 
-    document.getElementById('preview').src = `https://github.com/AstroSpaceMoth/Website/blob/master/${selected_image}`
+    document.getElementById('preview').src = `../${selected_image}`
     filename = selected_image
 }
