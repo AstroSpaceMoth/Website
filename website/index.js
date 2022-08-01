c6e1013ff882a0e854e39ebef1a5429ab46c257a
Jake <astrospacemoth@gmail.com>
2022-07-31 22:19:03 -0400

    launch commit

---
+++ website/index.js
@@ -0,0 +1,23 @@
+image_array = [
+    'WebDuck1.webm',
+    'WebDuck2.webm',
+    'WebDuck3.webm',
+    'WebDuck4.webm',
+    'WebDuck4.webm',
+    'WebDuck5.webm',
+    'WebDuck6.webm',
+    'WebDuck7.webm',
+    'WebDuck8.webm',
+    'WebDuck9.webm',
+]
+
+var filename = selected_image
+
+body.onload = function get_random_image(){
+    random_index = Math.floor(Math.random() * image_array.length);
+
+    selected_image = image_array[random_index]
+
+    document.getElementById('preview').src = `../${selected_image}`
+    filename = selected_image
+}
